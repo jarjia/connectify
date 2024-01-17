8b1c0e453c493c678e9b5a52b3ae69f4b2cbe377
jarji <100996174+jarjia@users.noreply.github.com>
2024-01-17 21:13:59 +0400

    feat: add layouts

---
+++ src/layouts/AuthLayout/AuthLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid grid-cols-[20%_60%_20%] auth-mid:grid-cols-1 p-2 h-screen">
+    <div class="auth-mid:hidden block"></div>
+    <div class="px-8 py-2 auth-sm:px-2">
+      <slot />
+    </div>
+    <div class="auth-mid:hidden block"></div>
+  </div>
+</template>
