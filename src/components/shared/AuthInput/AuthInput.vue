c1e2a159555ab5b1770ca9bfb4239bc20d411dbe
jarji <100996174+jarjia@users.noreply.github.com>
2024-01-17 21:15:15 +0400

    feat: add components

---
+++ src/components/shared/AuthInput/AuthInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  label: String,
+  id: String,
+  type: String,
+  placeholder: String,
+});
+</script>
+
+<template>
+  <div class="flex flex-col my-2 w-full">
+    <label :for="id">{{ label }}</label>
+    <input
+      :type="type"
+      :id="id"
+      :placeholder="placeholder"
+      class="border-2 py-1 px-2 mt-1 border-black outline-none rounded-sm"
+    />
+  </div>
+</template>
