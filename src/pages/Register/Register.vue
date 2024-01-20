19e28f55996a1d117e1910a13a922089c88ebf88
jarji <100996174+jarjia@users.noreply.github.com>
2024-01-20 12:41:13 +0400

    feat: make auth pages functional

--- src/pages/Register/Register.vue
+++ src/pages/Register/Register.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { AuthLayout } from "@/layouts";
-import { GoogleIcon, FacebookIcon, AuthInput } from "@/components";
 import { RouterLink } from "vue-router";
+import { GoogleIcon, FacebookIcon, AuthInput, Spinner } from "@/components";
+import useRegister from "./useRegister";
+
+const { dial_codes, onSubmit, phone, showSideBar, handleDropDown, isPending } =
+  useRegister();
 </script>
 
 <template>
@@ -25,37 +29,69 @@ import { RouterLink } from "vue-router";
         <GoogleIcon /> Google რეგისტრაცია
       </button>
     </div>
-    <form class="py-4 pt-2">
+    <form @submit="onSubmit" class="py-4 pt-2">
       <AuthInput
         type="text"
         id="name"
+        name="name"
         label="სახელი"
         placeholder="თქვენი სახელი, გვარი(ნებაყოფლობითია)"
       />
-      <AuthInput
-        type="tel"
-        id="tel"
-        label="ტელეფონი"
-        placeholder="მიუთითეთ თქვენი ტელეფონი"
-      />
-      <div class="flex w-full gap-6 auth-mid:flex-col auth-mid:gap-0">
+      <div class="flex flex-col mt-2 w-full">
+        <div class="flex w-full">
+          <AuthInput
+            type="tel"
+            id="tel"
+            name="phone"
+            label="ტელეფონი"
+            placeholder="თქვენი ტელეფონის ნომერი"
+            :handleDropDown="handleDropDown"
+            :phone="phone"
+          />
+        </div>
+      </div>
+      <div v-if="showSideBar" class="dropdown-container">
+        <div class="relative">
+          <ul
+            class="absolute max-h-52 overflow-y-scroll scrollbar bg-white shadow-2xl"
+          >
+            <li
+              v-for="item in dial_codes"
+              @click="
+                () => {
+                  showSideBar = !showSideBar;
+                  phone = item.dial_code;
+                }
+              "
+              class="cursor-pointer hover:bg-blue-400 hover:text-white px-2 py-[1px]"
+            >
+              {{ item.name }} {{ item.dial_code }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="flex w-full gap-6">
         <AuthInput
           type="password"
           id="password"
+          name="password"
           label="პაროლი"
           placeholder="შეიყვანეთ რთული პაროლი"
         />
         <AuthInput
           type="password"
           id="c_password"
+          name="c_password"
           label="გაიმეორეთ პაროლი"
           placeholder="გაიმეორეთ პაროლი"
         />
       </div>
       <button
-        class="py-2 w-full text-center text-white bg-blue-400 rounded mt-3"
+        type="submit"
+        class="py-2 w-full text-center h-10 text-white bg-blue-400 rounded mt-3"
       >
-        რეგისტრაცია
+        <Spinner v-if="isPending" />
+        <span v-else>შემოგვიერთდით!</span>
       </button>
       <p class="text-sm text-center mt-0.5">
         უკვე გაქვთ ანგარიში?
